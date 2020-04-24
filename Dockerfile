225ae2a769474524bb31bfc09f705894fe6211e6
Wesley Van Melle <van.melle.wes@gmail.com>
2020-04-23 17:13:34 -0700

    Fix major issues:
    * Versions were outputting ranges when not pinned
    * dependecy manifest paths were relative
    * pkg files were based on the current root, instead of pkg root
    * Entire crates were being downloaded for version numbers
    * Docker test was too complex
    (I can say that, I wrote it)

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
-FROM rust as builder
-
-WORKDIR /tc-build
-COPY src/ src/
-COPY Cargo.* ./
-
-RUN cargo build --release --locked
-
 FROM rust
-
-COPY --from=builder /tc-build/target/release/srclib-rust /usr/local/bin/
 
 RUN apt-get update && \
     apt-get upgrade -y && \
     apt-get install -y git
 
-WORKDIR /tc-test
+WORKDIR /tc-build
+COPY src/ src/
+COPY Cargo.* ./
 
-COPY test-me.sh ./
-
-CMD [ "/tc-test/test-me.sh" ]
+CMD cargo run -- scan --repo x --subdir .
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::error::Error as StdError;
 use std::path::{Path, PathBuf};
 use std::process;
 use std::result::Result as StdResult;
 
-use anyhow::{anyhow, Result};
-use cargo_metadata::{Dependency, Metadata, MetadataCommand, Package};
+use anyhow::{anyhow, Error, Result};
+use cargo_metadata::{Dependency, Metadata, MetadataCommand, Package, PackageId};
+use lazy_static::lazy_static;
+use regex::Regex;
+use semver::Version;
 use structopt::StructOpt;
 
 mod types;
@@ -28,6 +32,15 @@ enum CmdRoot {
     Scan(ParsedArgs),
 }
 
+fn extract_pkg_version(id: &PackageId) -> Version {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\d+\.\d+\.\d+").unwrap();
+    }
+
+    // Unwrapping is safe due to cargo guarantees
+    RE.find(&id.repr).unwrap().as_str().parse().unwrap()
+}
+
 fn get_manifest_path<P: AsRef<Path>>(root: &P) -> PathBuf {
     root.as_ref().join(MANIFEST_FILE)
 }
@@ -41,26 +54,29 @@ fn parse_args() -> ParsedArgs {
 fn parse_cargo_meta(args: &ParsedArgs) -> Result<Metadata> {
     MetadataCommand::new()
         .manifest_path(get_manifest_path(&args.subdir)) // use subdir arg
-        .no_deps() // Don't get dependency meta
-        .other_options(&["--offline".into()]) // don't make network calls
+        // .no_deps() // Don't get dependency meta
+        // .other_options(&["--frozen".into()]) // don't make network calls
         .exec()
         .map_err(|e| e.into())
 }
 
 fn xform_meta(meta: Metadata, args: &ParsedArgs) -> Result<Vec<SourceUnit>> {
     let self_packages = extract_self_packages(&meta);
-
-
+    let reference_path = args.subdir.canonicalize()?;
 
     self_packages
         .iter()
         .map(|this_package| {
+            // This is safe to unwrap/expect, since the manifest is a file
+            // and all files have a parent directory.
+            let package_dir = this_package.manifest_path.parent().expect("Unexpected manifest path");
+
             Ok(SourceUnitBuilder::default()
                 .name(this_package.name.to_owned())
                 .unit_type(UNIT_TYPE.to_owned())
                 .repo(Some(args.repo.clone()))
-                .files(get_source_files(&args.subdir)?)
-                .deps(Some(get_direct_deps(&this_package, args)?))
+                .files(get_source_files(package_dir, &reference_path.as_path())?)
+                .deps(Some(get_direct_deps(&this_package, &meta, package_dir)?))
                 .data(this_package.license.to_owned().map(Into::<License>::into))
                 .build()
                 .map_err(|e| anyhow!(e))?)
@@ -79,18 +95,31 @@ fn extract_self_packages(meta: &Metadata) -> Vec<&Package> {
         .collect()
 }
 
-fn get_direct_deps(pkg: &Package, args: &ParsedArgs) -> Result<Vec<ResolvedDependency>> {
+fn get_direct_deps<P: AsRef<Path>>(pkg: &Package, meta: &Metadata, root: P) -> Result<Vec<ResolvedDependency>> {
+    let resolved = meta.resolve.as_ref().ok_or_else(|| anyhow!("Could not locate dependency information"))?;
+    let root_node = resolved.nodes
+        .iter()
+        .find(|node| node.id == pkg.id)
+        .ok_or_else(|| anyhow!(format!("Could not locate root node for package: {}", pkg.name)))?;        
+
     pkg.dependencies
         .iter()
         .cloned()
-        .map(|dep| try_dep_xform(dep, args))
+        .map(|dep| {
+            let version_candidates: Vec<Version> = root_node.dependencies
+            .iter()
+            .filter(|id| id.repr.starts_with(&dep.name))
+            .map(extract_pkg_version)
+            .collect();
+            try_dep_xform(dep, &root, &version_candidates)
+        })
         .collect()
 }
 
-fn try_dep_xform(dep: Dependency, args: &ParsedArgs) -> Result<ResolvedDependency> {
+fn try_dep_xform<P: AsRef<Path>>(dep: Dependency, root: P, versions: &[Version]) -> Result<ResolvedDependency> {
     ResolvedDependencyBuilder::default()
-        .name(dep.name)
-        .version(Some(dep.req.to_string()))
+        .name(dep.name.clone())
+        .version(versions.iter().find(|version| dep.req.matches(version)).map(ToString::to_string))
         .optional(dep.optional)
         .source(
             dep.source
@@ -99,22 +128,53 @@ fn try_dep_xform(dep: Dependency, args: &ParsedArgs) -> Result<ResolvedDependenc
         .scope(Some("normal".into()))
         .default_features(dep.uses_default_features)
         .features(dep.features)
-        .cargo_toml_path(Some(get_manifest_path(&args.subdir).display().to_string()))
+        .cargo_toml_path(Some(get_manifest_path(&root).canonicalize()?.display().to_string()))
         .platform(dep.target.map(|p| p.to_string()))
         .build()
         .map_err(|e| anyhow!(e))
 }
 
-fn get_source_files<P: AsRef<Path>>(root: P) -> Result<Vec<PathBuf>> {
+fn get_source_files<P: AsRef<Path>>(root: P, reference_path: P) -> Result<Vec<PathBuf>> {
     glob::glob(&root.as_ref().join("**/*.rs").display().to_string())?
-        // Below is just an inverter between result and vec:
-        // Vec<Result<PathBuf, GlobError>> -> Result<Vec<PathBuf>, anyhow::Error>
-        .collect::<StdResult<Vec<_>, _>>()
-        .map_err(|e| e.into())
+        .map(|path| canonicalize_path(path, &reference_path))
+        .collect()
+}
+
+fn canonicalize_path<E, P>(rp: StdResult<PathBuf, E>, reference: P) -> Result<PathBuf>
+where
+    E: StdError + Send + Sync + 'static,
+    P: AsRef<Path>,
+{
+    rp.map(|p| {
+        p.strip_prefix(reference.as_ref())
+            // Unwrapping is safe because we only pick up files from within the root
+            .unwrap()
+            .to_owned()
+    })
+    .map_err(Into::<Error>::into)
+}
+
+fn ensure_index<P: AsRef<Path>>(root: P) -> Result<()> {
+    let success = process::Command::new("cargo")
+        .arg("generate-lockfile")
+        .current_dir(root)
+        .status()?
+        .success();
+
+    if !success {
+        Err(anyhow!("An error occurred while generating the lockfile"))
+    } else {
+        Ok(())
+    }
 }
 
 fn inner_main() -> Result<String> {
     let parsed = parse_args();
+    // Using `cargo generate-lockfile`, we can update the cached index cheaply
+    // without downloading whole crates.  This saves a lot of time and network
+    // I/O. This takes a extra few seconds on cache hits, but can save tens of
+    // minutes on misses.
+    ensure_index(&parsed.subdir)?;
     let cargo = parse_cargo_meta(&parsed)?;
     let units = xform_meta(cargo, &parsed)?;
     serialize_units(&units)
