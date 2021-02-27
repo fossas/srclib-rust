1623264495a3b4b44c3b13b6d4843db3d1aa3d19
Wesley Van Melle <van.melle.wes@gmail.com>
2021-02-26 16:58:00 -0800

    add the damn file
    Signed-off-by: Wesley Van Melle <van.melle.wes@gmail.com>

---
+++ src/dummy_main.rs
@@ -0,0 +1,4 @@
+// This file is used to allow docker builds to pre-build the dependency cache
+// before adding real source code to the image and running the real build.
+// It is ignored for normal builds.
+fn main() {}
